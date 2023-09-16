efc0c9a0a0d29494bb7ccc2b344658ba3db8c4de
Brandon Elzy <brandon.g.elzy@gmail.com>
2023-09-16 10:30:56 -0500

    Completed Assignment

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+/**
+* Create a program that prints a person's information
+*  1. Print the person's first name
+*  2. Print the person's last name on a new line
+*  3. Print the person's street address on a new line
+*  4. Print the person's city on a new line
+*  5. Print the person's zip code on a new line
+*  6. Exit program
+*/
+class Main {
+  public static void main(String[] args) {
+    // 1. Print the person's first name.
+    System.out.println("John");
+
+    // 2. Print the person's last name on a new line.
+    System.out.println("Doe");
+
+    // 3. Print the person's street address on a new line.
+    System.out.println("1234 Lantern Drive");
+
+    // 4. Print the person's city on a new line.
+    System.out.println("Chicago");
+
+    // 5. Print the person's zip code on a new line.
+    System.out.println("12345");
+  }
+}
